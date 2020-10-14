c10e2608e4aa3fc4d1ddbb1b307c48a36042a680
hjpark <gpwls2440@naver.com>
2020-10-14 16:29:55 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '../components/MainHeader'
+import Footer from '../components/MainFooter'
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  created() {
+    this.apitest()
+  },
+  methods: {
+    apitest() {
+      axios.get('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
+        console.log(res.data)
+      })
+    },
+  },
+}
+</script>
