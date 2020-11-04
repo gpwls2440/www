ec35dcdc28d6e451d3f73361955847e1be45026e
hjpark <gpwls2440@naver.com>
2020-11-04 13:48:31 +0900

    마퀴적용

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="container">
+    <MARQUEE>
+      <span>참외: 1400원</span>
+      <span>사과: 5400원</span>
+      <span>귤: 500원</span>
+      <span>메론: 9800원</span>
+      <span>배: 10900원</span>
+      <span>파인애플: 7000원</span>
+      <span>딸기: 14000원</span>
+      <span>체리: 3000원</span>
+      <span>레몬: 800원</span>
+      <span>복숭아: 6500원</span>
+    </MARQUEE>
     <router-link to="/chart/piechart">파이차트</router-link>
+    <router-link to="/chart/barchart">bar 차트</router-link>
     <router-link to="/chart">차트</router-link>
     <router-link to="/chart/trade">trade 차트</router-link>
     <router-link to="/keyword">키워드</router-link>
