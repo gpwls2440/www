841825e60830b7dfe291994ffc71b55adfae5f03
hjpark <gpwls2440@naver.com>
2020-12-09 16:50:11 +0900

    모달

---
+++ pages/CommonModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="modal-window" style="position: fixed; width: 100%; height: 100%; top: 0px; left: 0px; z-index: 1050; overflow: auto; background-color: rgba(0, 0, 0, 0.35)">
+    <div class="modal-box modal-type-inverted modal-size-normal modal-theme-atlant" style="position: absolute; top: 50%; left: 50%; margin-top: -85.5px; margin-left: -1066.5px">
+      <div class="modal-inner">
+        <div class="modal-title">
+          <h3>KDEX</h3>
+          <a class="modal-close-btn"></a>
+        </div>
+        <div class="modal-text">서비스 준비중입니다.</div>
+        <div class="modal-buttons"><a class="modal-btn btn-light-blue">예</a></div>
+      </div>
+    </div>
+  </div>
+</template>
