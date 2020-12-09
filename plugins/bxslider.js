0dd648b0d9474f987501047098b523521c5fb54f
hjpark <gpwls2440@naver.com>
2020-12-09 14:11:45 +0900

    bxslider css 제거

--- plugins/bxslider.js
+++ plugins/bxslider.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
 import bxslider from 'bxslider/dist/jquery.bxslider.min.js'
-import bxsliderCss from 'bxslider/dist/jquery.bxslider.css'
 
-Vue.use(bxslider, bxsliderCss)
+Vue.use(bxslider)
