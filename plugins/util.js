a0e3c0286c9999d73fa0b59519a66aade719bef8
hjpark <gpwls2440@naver.com>
2021-01-28 10:56:01 +0900

    utils 폴더 삭제(중복) plugins/util에 getSymbol getMarket 추가

--- plugins/util.js
+++ plugins/util.js
@@ -7,3 +7,13 @@ export const repUnComma = num => {
   num = String(num)
   return num.replace(/[^\d]+/g, '')
 }
+
+export const getSymbol = value => {
+  const arr = value.split('_')
+  return arr[0]
+}
+
+export const getMarket = value => {
+  const arr = value.split('_')
+  return arr[1]
+}
--- utils/util.js
+++
@@ -1,9 +0,0 @@
-export const Comma = num => {
-  num = String(num)
-  return num.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,')
-}
-
-export const UnComma = num => {
-  num = String(num)
-  return num.replace(/[^\d]+/g, '')
-}
